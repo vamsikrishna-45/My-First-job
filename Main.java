1d92ca6deeb32d2ffc2b1f6f859dbcacaae916d5
vamsikrishna-45 <mrvamsi777@gmail.com>
2026-04-08 12:25:54 +0530

    Add initial Main class with Hello World output

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // Simple variables
+        String appName = "MyApp";
+        int version = 1;
+
+        System.out.println("App Name: " + appName);
+        System.out.println("Version: " + version);
+    }
+}
